748547b19cb31bd9a90ab425b62a712c21cda853
Fairuzizuan Azzuri <fairuzizuan019@gmail.com>
2025-10-11 13:50:46 +0700

    automated commit

--- starter.sh
+++ starter.sh
@@ -3,21 +3,21 @@
 export STARTER_DIR=$(pwd)
 
 chmod +x $STARTER_DIR/{starter,commit}.sh
-source $STARTER_DIR/starter.sh
 
-
-echo "Clone alacritty-themes"
+echo "=== Clone alacritty-themes ==="
 mkdir -p ~/.config/alacritty/themes
 git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
 
-echo "Clone tpm"
+echo "=== Clone tpm ==="
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-echo "copying nvim config"
-ln -s $STARTER_DIR/config/nvim/ ~/.config/
+echo "=== Copying nvim config ==="
+ln -sf $STARTER_DIR/config/nvim/ ~/.config/
 
-echo "copying alacritty config"
-ln -s $STARTER_DIR/config/.alacritty.toml ~/
+echo "=== Copying alacritty config ==="
+ln -sf $STARTER_DIR/config/.alacritty.toml ~/
 
-echo "copying tmux config"
-ln -s $STARTER_DIR/config/.tmux.conf ~/
+echo "=== Copying tmux config ==="
+ln -sf $STARTER_DIR/config/.tmux.conf ~/
+
+echo "[✓] Done."
